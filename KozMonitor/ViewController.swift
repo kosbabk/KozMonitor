471a295711ea3cba00b032567bb78698ac21f7e4
Kelvin Kosbab <kelvin@kosbab.com>
2017-02-18 12:10:53 -0800

    Initial Commit

---
+++ KozMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KozMonitor
+//
+//  Created by Kelvin Kosbab on 2/18/17.
+//  Copyright © 2017 Kozinga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
